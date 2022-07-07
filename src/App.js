c34e2ba735584c4bd08d730b150e5c2ade9579c4
DannyMtwenty <dannywanyoike@gmail.com>
2022-07-07 15:17:22 +0300

    header setUP

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from './Home'
+import Header from './Header'
+import Banner from './Banner'
+
+function App() {
+    return (
+        //Bem
+        <div className = "App" >
+        <Header />
+        <Banner /> 
+       </div>
+    );
+}
+
+export default App;
